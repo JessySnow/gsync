8f75df0fc44674945883b57cc757d33d915c2704
jessy <jessysnow12345@gmail.com>
2024-06-28 10:57:32 +0800

    feat: add new config

--- config/parser_test.go
+++ config/parser_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var testConfig = Config{10, 5, []Repo{{"owner1", "repo1"}, {"owner2", "repo2"}}}
+var testConfig = Config{"/tmp", 10, 5, []Repo{{"owner1", "repo1"}, {"owner2", "repo2"}}}
 
 func TestParse(t *testing.T) {
 	type args struct {
@@ -17,7 +17,7 @@ func TestParse(t *testing.T) {
 		wantConfig *Config
 		wantErr    bool
 	}{
-		{wantConfig: &testConfig, args: args{bytes: []byte("{\n  \"sync_interval\": 10,\n  \"sync_gap\": 5,\n  \"repos\": [\n    {\n      \"owner\": \"owner1\",\n      \"repoName\": \"repo1\"\n    },\n    {\n      \"owner\": \"owner2\",\n      \"repoName\": \"repo2\"\n    }\n  ]\n}")}},
+		{wantConfig: &testConfig, args: args{bytes: []byte("{\n  \"root_dir\": \"/tmp\",\n \"sync_interval\": 10,\n  \"sync_gap\": 5,\n  \"repos\": [\n    {\n      \"owner\": \"owner1\",\n      \"repoName\": \"repo1\"\n    },\n    {\n      \"owner\": \"owner2\",\n      \"repoName\": \"repo2\"\n    }\n  ]\n}")}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- config/structs.go
+++ config/structs.go
@@ -2,6 +2,7 @@ package config
 
 // Config config of gsync
 type Config struct {
+	RootDir      string `json:"root_dir"`
 	SyncInterval int    `json:"sync_interval"`
 	SyncGap      int    `json:"sync_gap"`
 	Repos        []Repo `json:"repos"`
