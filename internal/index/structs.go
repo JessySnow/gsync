125f91f344da468a0e92a6a878ff78d1df2bf8ca
jessy <jessysnow12345@gmail.com>
2024-07-14 19:21:12 +0800

    feat: add an index interface, temporary rename origin index to bp_index

--- internal/index/structs.go
+++
@@ -1,8 +0,0 @@
-package index
-
-// DirNode an abstract representation of a path in a file system.
-type DirNode struct {
-	DirName      string   `json:"dir_name"`       // dirname from config's RootDir
-	Locked       bool     `json:"locked"`         // indicates a dir been locked
-	SubFileNames []string `json:"sub_file_names"` // sub files name
-}
