d98187946d18c36942c59b99c7b2bd17d39d6686
jessy <jessysnow12345@gmail.com>
2024-07-14 11:40:34 +0800

    feat: add enhance b-plus tree function and structs

---
+++ internal/index/enhance-bptree.go
@@ -0,0 +1,91 @@
+package index
+
+import (
+	"errors"
+	"fmt"
+	"github.com/collinglass/bptree"
+)
+
+// bptreeOrder bptree.node's order see bptree.tree.defaultOrder
+const bptreeOrder = 4
+
+var (
+	bptreeRootNil = errors.New("bptree's root node is nil")
+	noMoreElement = errors.New("no more element to return")
+)
+
+type enhanceBptree struct {
+	*bptree.Tree
+}
+
+type enhanceBptreeLeafNode struct {
+	*bptree.Node
+}
+
+type leafNodeKeyIterator struct {
+	index int
+	node  *enhanceBptreeLeafNode
+}
+
+func newEnhanceBptree() *enhanceBptree {
+	innerTree := bptree.NewTree()
+	return &enhanceBptree{innerTree}
+}
+
+// findLeaf get a leaf node which may contains key and wrap it in enhanceBptreeLeafNode
+func (e *enhanceBptree) findLeaf(key int) (leaf *enhanceBptreeLeafNode, err error) {
+	i := 0
+	if e.Root == nil {
+		return nil, bptreeRootNil
+	}
+	leaf = &enhanceBptreeLeafNode{e.Root}
+
+	for !leaf.IsLeaf {
+		i = 0
+		for i < leaf.NumKeys {
+			if key >= leaf.Keys[i] {
+				i += 1
+			} else {
+				break
+			}
+		}
+		leaf = &enhanceBptreeLeafNode{leaf.Pointers[i].(*bptree.Node)}
+	}
+	return leaf, nil
+}
+
+func (e *enhanceBptreeLeafNode) newIterator() (iterator *leafNodeKeyIterator, err error) {
+	if !e.IsLeaf {
+		return nil, fmt.Errorf("not a leaf node")
+	}
+
+	return &leafNodeKeyIterator{0, e}, nil
+}
+
+func (it *leafNodeKeyIterator) hasNext() bool {
+	if nil == it.node {
+		return false
+	}
+
+	if it.index >= it.node.NumKeys && nil == it.node.Pointers[bptreeOrder-1] {
+		return false
+	} else {
+		return true
+	}
+}
+
+func (it *leafNodeKeyIterator) getNext() (key int, err error) {
+	if it.hasNext() {
+		if it.index < it.node.NumKeys {
+			key = it.node.Keys[it.index]
+			it.index += 1
+			return key, nil
+		} else {
+			it.index = 0
+			it.node = &enhanceBptreeLeafNode{it.node.Pointers[bptreeOrder-1].(*bptree.Node)}
+			return it.getNext()
+		}
+	} else {
+		return key, noMoreElement
+	}
+}
---
+++ internal/index/enhance-bptree_test.go
@@ -0,0 +1,60 @@
+package index
+
+import (
+	"testing"
+)
+
+func Test_leafNodeKeyIterator_getNext(t *testing.T) {
+	tree := newEnhanceBptree()
+	_ = tree.Insert(1, nil)
+	_ = tree.Insert(2, nil)
+	_ = tree.Insert(3, nil)
+	_ = tree.Insert(4, nil)
+	_ = tree.Insert(5, nil)
+	_ = tree.Insert(6, nil)
+	_ = tree.Insert(7, nil)
+	leaf, _ := tree.findLeaf(1)
+	iterator, _ := leaf.newIterator()
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	for _, i := range nums {
+		get, _ := iterator.getNext()
+		if i != get {
+			t.Errorf("iterator get failed, except: %d, get: %d", i, get)
+			return
+		}
+	}
+
+	if iterator.hasNext() {
+		t.Errorf("iterator hasNext check failed, except: %v, get: %v", false, true)
+	}
+}
+
+func Test_leafNodeKeyIterator_hasNext(t *testing.T) {
+	tree := newEnhanceBptree()
+	_ = tree.Insert(1, nil)
+	_ = tree.Insert(2, nil)
+	_ = tree.Insert(3, nil)
+	_ = tree.Insert(4, nil)
+	_ = tree.Insert(5, nil)
+	_ = tree.Insert(6, nil)
+	_ = tree.Insert(7, nil)
+	_ = tree.Insert(8, nil)
+	leaf, _ := tree.findLeaf(1)
+	iterator, _ := leaf.newIterator()
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	for _, i := range nums {
+		get, _ := iterator.getNext()
+		if i != get {
+			t.Errorf("iterator get failed, except: %d, get: %d", i, get)
+			return
+		}
+	}
+
+	if !iterator.hasNext() {
+		t.Errorf("iterator hasNext check failed, except: %v, get: %v", false, true)
+	}
+	_, _ = iterator.getNext()
+	if iterator.hasNext() {
+		t.Errorf("iterator hasNext check failed, except: %v, get: %v", false, true)
+	}
+}
